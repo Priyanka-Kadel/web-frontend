0d2b93fd2642aa679b2403ee02dd0dec0b6efceb
Priyanka-Kadel <kadelpriyanka21@gmail.com>
2025-03-01 13:10:55 +0545

    final changes in public dashboard ui

--- src/components/public/PublicDashboard.jsx
+++ src/components/public/PublicDashboard.jsx
@@ -95,7 +95,7 @@ const PublicDashboard = () => {
 
       {/* Product Grid Section */}
       <section className="py-16 px-6 md:px-20">
-        <h2 className="text-xl font-extrabold mb-8 text-[#00693e] text-center">Choose the Gears You Need</h2>
+        <h2 className="text-3xl font-extrabold mb-8 text-[#00693e] text-center">Choose the Gears You Need</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
           {products.map((product) => (
             <motion.div
